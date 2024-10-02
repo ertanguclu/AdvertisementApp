3f46aca91ace38eab33faf2dc1d7106c995e3e71
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-10-02 14:05:51 +0300

    Added Advertisement and AdvertisementAppUser Config

---
+++ Udemy.AdvertisementApp.DataAccess/Configurations/AdvertisementConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Udemy.AdvertisementApp.Entities;
+
+namespace Udemy.AdvertisementApp.DataAccess.Configurations
+{
+    public class AdvertisementConfig : IEntityTypeConfiguration<Advertisement>
+    {
+        public void Configure(EntityTypeBuilder<Advertisement> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Description).HasColumnType("ntext").IsRequired();
+            builder.Property(x => x.CreateDate).HasDefaultValueSql("getdate()");
+        }
+    }
+}
