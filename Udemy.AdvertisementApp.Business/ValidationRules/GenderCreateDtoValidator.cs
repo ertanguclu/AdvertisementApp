a8cb3ba6cd644c3ec212bae20241424598079c9c
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-10-05 14:05:13 +0300

    Added gender create and update validation rules

---
+++ Udemy.AdvertisementApp.Business/ValidationRules/GenderCreateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Udemy.AdvertisementApp.Dtos;
+
+namespace Udemy.AdvertisementApp.Business.ValidationRules
+{
+    public class GenderCreateDtoValidator : AbstractValidator<GenderCreateDto>
+    {
+        public GenderCreateDtoValidator()
+        {
+            RuleFor(x => x.Definition).NotEmpty();
+        }
+    }
+}
---
+++ Udemy.AdvertisementApp.Business/ValidationRules/GenderUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Udemy.AdvertisementApp.Dtos;
+
+namespace Udemy.AdvertisementApp.Business.ValidationRules
+{
+    public class GenderUpdateDtoValidator : AbstractValidator<GenderUpdateDto>
+    {
+        public GenderUpdateDtoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Definition).NotEmpty();
+        }
+    }
+}
